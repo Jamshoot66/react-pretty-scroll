b312f0240032bb37d0f71aeb962f7f0642da856f
Sergey Gorshkov <gorshkov88@gmail.com>
2020-08-28 15:40:18 +0400

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import PrettyScroll from './package/PrettyScroll';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import PrettyScroll from "./package/PrettyScroll";
 
 function App() {
   return (
