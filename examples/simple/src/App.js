4944e46fd09e49f049cf0e4aeaf76edfe3a04c08
Sergey Gorshkov <gorshkov88@gmail.com>
2020-08-28 15:15:51 +0400

    add simple example

---
+++ examples/simple/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import PrettyScroll from 'react-pretty-scroll';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PrettyScroll title='QWE'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
