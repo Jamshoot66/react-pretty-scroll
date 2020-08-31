43afe342339d2405e6a318349ee246fa5374321f
Sergey Gorshkov <gorshkov88@gmail.com>
2020-08-31 17:38:18 +0400

    clean useless files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 import Playground from "./components/Playground";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Playground />
     </div>
   );
