eb11a493eb590f114ddd89d351f8ec4c150ca02e
Sergey Gorshkov <gorshkov88@gmail.com>
2020-09-04 11:40:01 +0400

    chore simple example

--- examples/simple/src/App.js
+++ examples/simple/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
-import PrettyScroll from 'react-pretty-scroll';
+import React from "react";
+import "./App.css";
+import PrettyScroll from "react-pretty-scroll";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PrettyScroll title='QWE'/>
+        <PrettyScroll isVertical width={"500px"} height={"500px"} isDraggable>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              background: "#F00",
+              width: 300,
+              height: 1000,
+            }}
+          >
+            SOME
+          </div>
+        </PrettyScroll>
       </header>
     </div>
   );
